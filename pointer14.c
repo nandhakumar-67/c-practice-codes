e852a02c318221af8b221d5c44092f6e4184c02f
nandhakumar-67 <nandhu.nk6789@gmail.com>
2025-07-07 20:06:23 +0530

    this is called by arrray

--- pointer14.c
+++ pointer14.c
@@ -4,14 +4,15 @@ int nk(int *a,int size)
     int i,sum=0;
     for (i=0;i<size;i++){
         sum = sum + *a;
+        a++;
     }
     return sum;
 }
 int main()
 {
-    int a[10]={10,10,10,10,}
+    int a[10]={10,10,10,10};
     int size = sizeof a / sizeof a[0];
-    int toatal = nk(&a,size);
+    int toatal = nk(a,size);
     printf("%d", toatal);
 
 }
