90461b56ed721f3a057ae59d29c7c40e12492c57
nandhakumar-67 <nandhu.nk6789@gmail.com>
2025-07-08 23:23:05 +0530

    this is c program

--- pointer17.c
+++ pointer17.c
@@ -2,6 +2,12 @@
 #include<string.h>
 void print(char *ch)
 {
+    ch[6]= 's';
+    ch[7]='u';
+    ch[8]='p';
+    ch[9]='e';
+    ch[10]='r';
+    
    while(*ch!='\0')
    {
     printf("%c",*ch);
@@ -13,8 +19,6 @@ void print(char *ch)
 }
 int main()
 {
-    char c[]="nandhakumar";
-    char *ch;
-    ch = c;
+    char ch[]="nandhakumar";
     print(ch);
 }
